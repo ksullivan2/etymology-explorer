70871202e82c480c090dc674156ef97900ef2907
Kaley Sullivan <ksullivan@etsy.com>
2020-05-19 19:18:08 -0400

    click handler

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React from "react";
+import React, {Component} from "react";
 
 import Network from "./Network";
 import dictionary from "./dictionary.json"
 import links from "./links.json"
 
   
-function App() {
+class App extends Component {
 
-  return (
-    <div className="App">
-      <Network
-        width={800}
-        height={800}
-        network={{
-          nodes: dictionary.data,
-          links: links.data
-        }}
-      />
-    </div>
-  );
+  state = {
+    selectedNode: null
+  }
+
+  onNodeClick(nodeID) {
+    this.setState({selectedNode: nodeID});
+  }
+
+  render() {
+    //"word" is the data that gets passed to the individual node
+    let nodes = dictionary.data.map((node, i) => {
+        node.isSelected = node.id === this.state.selectedNode
+        node.onClick = this.onNodeClick.bind(this, node.id)
+        return node;
+    });
+
+    return (
+      <div className="App">
+        <Network
+          width={800}
+          height={800}
+          network={{
+            nodes: nodes,
+            links: links.data
+          }}
+        />
+      </div>
+    );
+  }
+
+  
 }
 
 export default App;
--- src/Word.js
+++ src/Word.js
@@ -2,18 +2,26 @@ import React from "react";
 
 // The node rendered by the graph
 class Word extends React.Component {
-  render() {
-  	let word = this.props.node;
-    return (
-		<g>
-			<circle r={40}  stroke={"#adadeb"} fill="white"/>
-			<text textAnchor="middle" dy="0.3em">
-				{word.prefix}
-				<tspan fill="#ff00ff">{word.root}</tspan>
-				{word.suffix}
-			</text>
-		</g>
-  )}
+
+	render() {
+	  	let word = this.props.node;
+	  	let outlineColor = word.isSelected
+    		? "black"
+    		: "#adadeb";
+	    return (
+			<g onClick={word.onClick}>
+				<circle r={40}  stroke={outlineColor} fill="white"/>
+				<text textAnchor="middle" dy="0.3em">
+					{word.prefix}
+					<tspan fill="#ff00ff">{word.root}</tspan>
+					{word.suffix}
+				</text>
+			</g>
+	)}
+
+  	
+
+
 }
 
 export default Word;
