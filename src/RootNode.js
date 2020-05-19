8fe4f81a35ae9c1e21a53dc203147ac08122a0f4
Kaley Sullivan <ksullivan@etsy.com>
2020-05-19 12:13:08 -0400

    THANK EVERYTHING THATS HOLY

---
+++ src/RootNode.js
@@ -0,0 +1,22 @@
+function RootNode({ node }) {
+  const width = 40;
+  const height = 20;
+  const centerX = -width / 2;
+  const centerY = -height / 2;
+
+  return (
+    <Group top={node.y} left={node.x}>
+      <rect width={width} height={height} y={centerY} x={centerX} fill="url('#top')" />
+      <text
+        dy={'.33em'}
+        fontSize={9}
+        fontFamily="Arial"
+        textAnchor={'middle'}
+        style={{ pointerEvents: 'none' }}
+        fill={bg}
+      >
+        {node.data.name}
+      </text>
+    </Group>
+  );
+}
