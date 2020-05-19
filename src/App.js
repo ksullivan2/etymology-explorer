e929ddfb00f55dbe5776d9ebece068ff0fd36330
Kaley Sullivan <ksullivan@etsy.com>
2020-05-19 19:55:41 -0400

    filters

--- src/App.js
+++ src/App.js
@@ -8,16 +8,32 @@ import links from "./links.json"
 class App extends Component {
 
   state = {
-    selectedNode: dictionary.data[1]
+    selectedNode: dictionary.data[dictionary.data.length - 1]
   }
 
   onNodeClick(node) {
     this.setState({selectedNode: node});
   }
 
+  filterWords(selectedNode) {
+    return dictionary.data.filter(word => {
+      return word.root === selectedNode.root
+    })
+    
+  }
+
+  filterLinks(selectedNode) {
+    return links.data.filter(link => {
+      return link.source === selectedNode.root || link.target === selectedNode.root
+    })
+  }
+
   render() {
+    let filteredWords = this.filterWords(this.state.selectedNode)
+    let filteredLinks = this.filterLinks(this.state.selectedNode)
+
     //"word" is the data that gets passed to the individual node
-    let nodes = dictionary.data.map((node, i) => {
+    let nodes = filteredWords.map((node, i) => {
         node.isSelected = this.state.selectedNode && node.id === this.state.selectedNode.id 
         node.onClick = this.onNodeClick.bind(this, node)
         return node;
@@ -27,11 +43,11 @@ class App extends Component {
       <div className="App">
         <DetailView word={this.state.selectedNode} />
         <Network
-          width={800}
+          width={1200}
           height={600}
           network={{
             nodes: nodes,
-            links: links.data
+            links: filteredLinks
           }}
         />
       </div>
